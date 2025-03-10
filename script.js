54198bc83adea0f28ec3a2265ab17b6162f49050
thembu <rakhivhanithembu@gmail.com>
2025-03-10 15:17:04 +0200

    Added error message

--- script.js
+++ script.js
@@ -7,69 +7,59 @@ let submitContent = () => {
     document.getElementById('country-info').innerHTML = '';
     document.getElementById('bordering-countries').innerHTML = '';
 
-    
-	fetch(`https://restcountries.com/v3.1/name/${input.value}`)
-		.then((res) => res.json())
-		.then((data) => {
+    fetch(`https://restcountries.com/v3.1/name/${input.value}`)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Country not found');
+            }
+            return res.json();
+        })
+        .then((data) => {
 
             let country = data[0];  
 
-			country_info =
-            
-            `
+            country_info = `
                 <ul>
-                <li>capital : ${country.capital[0]} </li>
-                <li>Region : ${country.region}</li>
-                <li>Population : ${country.population}</li>
-                <li> Flag : </li>
-                <img src="${country.flags.png}"></img>
-            </ul>
-    </section>
-            
-            `
+                    <li>Capital: ${country.capital[0]} </li>
+                    <li>Region: ${country.region}</li>
+                    <li>Population: ${country.population}</li>
+                    <li>Flag: </li>
+                    <img src="${country.flags.png}"></img>
+                </ul>
+            `;
 
             document.getElementById('country-info').innerHTML = country_info;
 
-            let  borderCountryCodes = country.borders; 
+            let borderCountryCodes = country.borders;
 
-
+            document.getElementById('bordering-countries').innerHTML = '';
 
             for (let i = 0; i < borderCountryCodes.length; i++) {
-                
                 let code = borderCountryCodes[i];
 
-
                 fetch(`https://restcountries.com/v3.1/alpha/${code}`)
-                .then((res) => res.json())
-                .then((data) => {
+                    .then((res) => res.json())
+                    .then((data) => {
 
+                        let boarderCountry = data[0];
+                        console.log(boarderCountry.name.common);
 
-                    let boarderCountry = data[0];
-                    console.log(boarderCountry.name.common)
+                        bordering_countries = ` 
+                            <p>${boarderCountry.name.common}</p>
+                            <img src="${boarderCountry.flags.png}"></img>
+                        `;
 
-                    bordering_countries = ` 
+                        document.getElementById('bordering-countries').innerHTML += bordering_countries;
+                    })
+                    .catch((err) => {
+                        console.error('Error fetching bordering country data:', err);
+                    });
+            }
 
-                    <p>
-                    ${boarderCountry.name.common}
-                    </p>
-
-                    <img src="${boarderCountry.flags.png}"></img>
-                
-                `
-
-                console.log(bordering_countries)
-
-                document.getElementById('bordering-countries').innerHTML += bordering_countries;
-            }).catch((err) => {
-                console.error('Error fetching country data:', error);
-                document.getElementById('country-info').innerHTML = 
-                    '<p>Country not found. Please try again.</p>';
-            
-            })
-        
-        
-        }
-
-
-
-        })}
+        })
+        .catch((err) => {
+            console.error('Error fetching country data:', err);
+            document.getElementById('country-info').innerHTML = 
+                '<p style="color:red;">Country not found. Please check the spelling or enter a valid country name.</p>';
+        });
+}
